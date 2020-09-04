d1cd0292d9ae1b989d6850d744e0a441b55bcde2
Ioannis Christodoulakis <48016428+JodaCh96@users.noreply.github.com>
2020-09-04 16:39:54 +0200

    Create Template.c

---
+++ Template.c
@@ -0,0 +1,56 @@
+/*******************************************************************************
+* Programm:
+* Filename:   .c
+*
+* Autor:      Ioannis Christodoulakis (IC)
+* Version:    1.0
+* Datum:
+*
+* Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt, Zeit):
+* 1.0
+*
+********************************************************************************
+*
+* Verwendungszweck: C-Schulung
+*
+* Beschreibung:
+*
+* Benötigte Libraries:
+* - stdlib.h
+* - stdio.h
+*
+* Copyright (c) 2013 by I.Christodoulakis, CH-6340 Baar
+*******************************************************************************/
+
+/***  Include Files ***********************************************************/
+#include <stdlib.h> /* Funktionsbibliothek: Hilfsfunktionen */
+#include <stdio.h>  /* Funktionsbibliothek: Standard Ein- Ausgabe */
+
+/***  Globale Deklarationen und Definitionen **********************************/
+
+
+/***  Funktions-Deklarationen *************************************************/
+
+
+/*******************************************************************************
+******************************* HAUPTPROGRAMM **********************************
+*******************************************************************************/
+int main(void) {
+
+  // lokale Variablen
+
+
+
+  /* Intro --------------------- */
+
+
+  /* Eingabe ------------------- */
+
+
+  /* Verarbeitung und Ausgabe -------------- */
+
+
+  /* Ausgabe ------------------- */
+
+  return (0);
+}
