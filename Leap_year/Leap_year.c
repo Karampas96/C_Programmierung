18601a993bc85fa04a97be36a6d95257f5bad2e1
Ioannis Christodoulakis <48016428+JodaCh96@users.noreply.github.com>
2020-09-04 15:55:55 +0200

    Update the leap year

--- Leap_year/Leap_year.c
+++ Leap_year/Leap_year.c
@@ -20,64 +20,29 @@ COPYRIGHT (C) 2020  by I.Christodoulakis  CH-6340 Baar
 /* VARIABLEN (global) ---------- */
 
 /* HAUPTPROGRAMM --------------- */
-int main()
- {
-  /* VARIABLEN (lokal) --------- */
-  int Jahreszahl;
-  int Schaltjahr;
-  int Restzahl;
-  
-  /* Intro --------------------- */
-  printf("***   SCHALTJAHR    ***\n\n\n");
-  
-  
-  /* Eingabe -------------------*/
-  Schaltjahr = 0;
-  
-  printf("Bitte geben sie eine Jahreszahl als Integerwert ein: ");
-  scanf( "%d", &Jahreszahl);
+int main() {
+   int year;
+   printf("Enter a year: ");
+   scanf("%d", &year);
 
-  Restzahl = Jahreszahl % 4;
+   // leap year if perfectly visible by 400
+   if (year % 400 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   // not a leap year if visible by 100
+   // but not divisible by 400
+   else if (year % 100 == 0) {
+      printf("%d is not a leap year.", year);
+   }
+   // leap year if not divisible by 100
+   // but divisible by 4
+   else if (year % 4 == 0) {
+      printf("%d is a leap year.", year);
+   }
+   // all other years are not leap year
+   else {
+      printf("%d is not a leap year.", year);
+   }
 
-  
-  if(Restzahl == 0)
-  {
-     Schaltjahr = 1;
-  } else
-  {
-     Schaltjahr = 0;
-  }
-
-  Restzahl = Jahreszahl % 100;
-
-  
-  if(Restzahl == 0)
-  {
-     Schaltjahr = 0;
-  } 
-
-  Restzahl = Jahreszahl % 400;
-
-  
-  if(Restzahl == 0)
-  {
-     Schaltjahr = 1;
-  } 
- 
-  if(Restzahl = 1)
-  {
-     printf("\n\nDas Jahr %d ist ein Schaltjahr\n\n" ,Jahreszahl);
-  } 
-  else
-  {
-     printf("\n\nDas Jahr %d ist kein Schaltjahr\n\n" ,Jahreszahl);
-  }
-  
-  system ("PAUSE"); /* Nur während der Entwicklungsphase, später löschen! */
-  
-  return 0;
- } /* ------------------------------------------------------------------------*/
-
-/* Bildschirmdialog
-
-*/
+   return 0;
+}
