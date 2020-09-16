5728149b255a2c3dfc0716a793c0f59b51e9e13d
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-09-16 15:08:27 +0200

    Update Histogram.c

--- Histogram/Histogram.c
+++ Histogram/Histogram.c
@@ -16,37 +16,37 @@
 
 /***  Global Variables ********************************************************/
 unsigned int count[256] = {0};
-
 /*******************************************************************************
 ******************************* HAUPTPROGRAMM **********************************
 *******************************************************************************/
-int main(int argc, char* argv[], char* envp[]){
+int main(int argc, char* argv[], char* envp[]) {
 	FILE* stream = NULL;
-	char *filename="Alice Adventure in Wonderland.txt";
+	_set_errno(0);
+	char* filename = "Alice Adventure in Wonderland.txt";
 	int c; /* a holder for each letter */
 	int k;
 	int max_letter = 0;
 	int char_count = 0;
-	errno_t err;
-	if(argc >= 2){
+
+	if (argc >= 2) {
 		filename = argv[1];
 	}
 	// Open File for read
-	err = fopen_s(&stream, filename, "r");
-	if (err != 0){
+	errno_t err = fopen_s(&stream, filename, "r");
+	if (err != 0) {
 		printf("\nDie Datei '%s' wurde nicht richtig geoeffnet.\n\n", filename);
 		return 1;
 	}
 	assert(stream != NULL);
-	while((c = fgetc(stream))){ //@RLA chave your condition => than you can get rid of the subsequent if!
-		if(c == EOF) break; // break if end of file
+	while ((c = getc(stream))) { //@RLA chave your condition => than you can get rid of the subsequent if!
+		if (c == EOF) break; // break if end of file
 		count[c]++;
-		if (count[c] > count[max_letter]){
+		if (count[c] > count[max_letter]) {
 			max_letter = c;
 		}
 	}
-	for(k = 0; k < 255; k++){
-		if(count[k] > 0){
+	for (k = 0; k < 255; k++) {
+		if (count[k] > 0) {
 			printf("Der Buchstabe '%c' kommt %d-mal vor.\n", k, count[k]);
 			char_count += count[k];
 		}// count letter
@@ -54,5 +54,6 @@ int main(int argc, char* argv[], char* envp[]){
 	printf("\nAnzahl Zeichen: %d \n\n", char_count);
 	printf("Max letter: '%c' -> %d\n", max_letter, count[max_letter]);
 	// file closed
-	fclose(filename);
+	fclose(stream);
+	return 0;
 }//main
