d8f2c8e6349ffe9d2a549ecd3daba39dfbf2f823
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-09-22 13:14:33 +0200

    Update main.c

--- Zeigeranalyse/Zeigeranalyse A/main.c
+++ Zeigeranalyse/Zeigeranalyse A/main.c
@@ -4,7 +4,7 @@ int main(){
 int ZahlA = 3, ZahlB = 5;
 int *PtrA = &ZahlA, *PtrB = &ZahlB, *PtrC = PtrA;
 
-printf("Zahl A = %u, ZahlB = %u, *PtrA = %u, *PtrB = %u, *PtrC = %u \n", ZahlA, ZahlB, *giPtrA, *PtrB, *PtrC);
+printf("Zahl A = %u, ZahlB = %u, *PtrA = %u, *PtrB = %u, *PtrC = %u \n", ZahlA, ZahlB, *PtrA, *PtrB, *PtrC);
 printf("Ausgabe 1 : %u \n", PtrA == &ZahlA);
 printf("Ausgabe 2 : %u \n", * * & PtrA);
 printf("Ausgabe 3 : %u \n", 7 * *PtrA / *PtrB + 7);
