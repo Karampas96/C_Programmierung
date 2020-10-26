fe16f675cca1e10bb7e0c9714305955a5d1ef1a8
Ioannis Christodoulakis <48016428+YodaCh96@users.noreply.github.com>
2020-10-26 10:51:15 +0100

    Update Vorlage.c

--- Vorlage.c
+++ Vorlage.c
@@ -1,10 +1,10 @@
 /*******************************************************************************
-* Programm:
+* Programm:   main
 * Filename:   main.c
 *
 * Autor:      Ioannis Christodoulakis (IC)
 * Version:    1.0
-* Datum:      04.09.2020
+* Datum:      
 *
 * Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt, Zeit):
 * 1.0
@@ -57,5 +57,40 @@ int main(void) {
 
   /* Ausgabe ------------------------------ */
 
+  system ("PAUSE"); /* Nur während der Entwicklungsphase, später löschen! */
   return (0);
 }
+
+/*******************************************************************************
+*************************** FUNKTIONS-DEFINITIONEN *****************************
+*******************************************************************************/
+
+/*******************************************************************************
+* Funktion:  
+* Source: 	  .c
+* Call:      	   			   
+*									   									   
+* Autor:     Ioannis Christodoulakis (IC)					  						   								   									   
+* Version:   1.0				   			   								   									   
+* Datum: 	   
+*
+* Entwicklungsablauf(Version, Datum, Autor, Entwicklungsschritt):
+* 1.0								   									   
+********************************************************************************
+*
+* Verwendungszweck: C-Schulung
+*
+* Beschreibung:
+* 
+* 
+* Precondition: -
+*
+* Postcondition: -
+*
+* Benötigte Unterprogramme: -
+*
+* Parameter: (I: Input, O: Output)
+* 
+*
+* Copyright (c) 2020 by I.Christodoulakis, CH-6340 Baar
+*******************************************************************************/
