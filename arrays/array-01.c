1e8fcf58abed5782f37c762116ea03cbcf375336
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-07-12 08:51:06 +0200

    Update array-01.c

--- arrays/array-01.c
+++ arrays/array-01.c
@@ -1,16 +1,15 @@
-/* array_1.c */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-	
-	int i[5];
-	double abc[2000];
-	
-	printf("%d\n", sizeof(i[0])); // 4 Bytes
-	printf("%d\n", sizeof(i)); // 20 Bytes
-	printf("%d\n", sizeof(abc[0])); // 8 Bytes
-	printf("%d\n", sizeof(abc)); // 16'000 Bytes
-	
-	return EXIT_SUCCESS;
+int main(void)
+{
+    int i[5];
+    double abc[2000];
+
+    printf("%d\n", sizeof(i[0])); // 4 Bytes
+    printf("%d\n", sizeof(i)); // 20 Bytes
+    printf("%d\n", sizeof(abc[0])); // 8 Bytes
+    printf("%d\n", sizeof(abc)); // 16'000 Bytes
+
+    return EXIT_SUCCESS;
 }
